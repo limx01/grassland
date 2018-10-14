f873f497928d17c9e80e982c8d9045662f6d3fd1
limingxing <limx01@missfresh.cn>
2018-10-15 00:06:18 +0800

    add test case

---
+++ grassland-api/src/main/java/me/limx/grassland/api/dto/OrderDto.java
@@ -0,0 +1,13 @@
+package me.limx.grassland.api.dto;
+
+import lombok.Data;
+
+/**
+ * @author: limingxing
+ * @date: 2018/10/14 23:56
+ */
+@Data
+public class OrderDto {
+    private Long id;
+    private Long orderId;
+}
